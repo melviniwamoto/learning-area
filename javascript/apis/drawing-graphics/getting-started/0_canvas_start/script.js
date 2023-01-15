db1e83a70700fb47140e950244c15837b7e8e2bf
melviniwamoto <122716501+melviniwamoto@users.noreply.github.com>
2023-01-15 12:31:23 -1000

    Update script.js

--- javascript/apis/drawing-graphics/getting-started/0_canvas_start/script.js
+++ javascript/apis/drawing-graphics/getting-started/0_canvas_start/script.js
@@ -1,4 +1,9 @@
+// The main object that javascript references
 const canvas = document.querySelector(".myCanvas");
+
+// Display dimensions
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
+
+// The object that javascript draws to
 const ctx = canvas.getContext("2d");
