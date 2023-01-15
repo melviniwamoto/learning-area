6870314523df9dd573bbdff1945cb82f4f954266
melviniwamoto <122716501+melviniwamoto@users.noreply.github.com>
2023-01-15 12:33:38 -1000

    Update script.js

--- javascript/apis/drawing-graphics/getting-started/0_canvas_start/script.js
+++ javascript/apis/drawing-graphics/getting-started/0_canvas_start/script.js
@@ -5,5 +5,11 @@ const canvas = document.querySelector(".myCanvas");
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-// The object that javascript draws to
+// The object that javascript draws with/to
 const ctx = canvas.getContext("2d");
+
+// Method to set fill style (color here)
+ctx.fillStyle = "rgb(0, 0, 0)";
+
+// Method specifying a rectangular fill area
+ctx.fillRect(0, 0, width, height);
